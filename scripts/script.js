0053d7bcfd0b1be31ecfd8d7397dec8bbba76ee5
Marco <marco.chianese93@gmail.com>
2020-11-20 16:34:16 +0100

    implementazione funzione stampa auto mail (da fixare)

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,19 @@
 var root = new Vue({
   el: '#root',
   data: {
-    message: 'You loaded this page on ' + new Date().toLocaleString(),
+    elenco_mail : [],
     },
     methods : {
 
     },
     mounted() {
-
+        const self = this ;
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then(function () {
+        .then (function (generated_mail) {
+            console.log(generated_mail.data);
+            console.log(generated_mail.data.response);
 
+        },
 
 
         })
